9fc86dd3789902bc961a29c1ee24557d87b505f0
Nadi Awad <nadiawad60@hotmail.com>
2016-07-31 18:16:11 +0100

    New samples are added

---
+++ Samples/JavaBasics/MultipleClassesInOneFile.java
@@ -0,0 +1,28 @@
+package JavaBasics;
+
+/**
+ * Created by Nadi on 31/07/2016.
+ */
+public class MultipleClassesInOneFile {
+}
+/*
+public class Class2{
+
+}
+
+public interface Interface1{
+
+}
+*/
+class Class3{
+
+}
+
+interface Interface2{
+
+}
+
+/*
+A Java source code file can define multiple interfaces and classes.
+A "public" interface/class can be defined in a Java source code file with a matching name. Therefore, a file cannot define more than one public class/interface.
+ */
