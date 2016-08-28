54affea8f9a701acd4ce25ce09aaded327f16ff6
Nadi Awad <nadiawad60@hotmail.com>
2016-08-28 19:46:18 +0100

    Added an example for Primitive Variables - char variables.

--- Samples/datatypes/PrimitiveVariables.java
+++ Samples/datatypes/PrimitiveVariables.java
@@ -31,7 +31,25 @@ public class PrimitiveVariables {
         //int num2=_123; //Compilation Error: Cannot start or end a literal value with an underscore
         //int num3=123_; //Compilation Error: Cannot start or end a literal value with an underscore
         //long num4=123_L; //Compilation Error: Cannot place an underscore before the suffix L
-        int num5=Integer.parseInt("123_456"); //Runtime Error: Invalid Use of underscore where a string of digits is expected
+        //int num5=Integer.parseInt("123_456"); //Runtime Error: Invalid Use of underscore where a string of digits is expected
 
+        System.out.println(binVal);
+        System.out.println(Integer.toBinaryString(binVal));
+
+        double floatingNum=1e2;
+        System.out.println(floatingNum);
+
+        //double doubleVal1=123_.456;//Compilation Error: cannot use underscore adjacent to a decimal point
+
+        char char1='a';
+        char char2=97;
+        char char3='\u9890';
+        System.out.println(char3);
+        if (char1==char2){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
     }
 }
